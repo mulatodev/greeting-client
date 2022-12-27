e002df673367a992a2b41accdaa279768739b93f
mulatodev <ganaranjo@gmail.com>
2022-12-27 09:49:25 -0500

    Greeting-client project initial commit

---
+++ src/main/java/xyz/mulatodev/greetingclient/GreetingClientApplication.java
@@ -0,0 +1,29 @@
+package xyz.mulatodev.greetingclient;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class GreetingClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingClientApplication.class, args);
+	}
+
+        @Bean
+        public RestTemplate restTemplate(RestTemplateBuilder builder){
+            return builder.build();
+        }
+        
+        @Bean
+        public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+            return arg -> {
+                String message = restTemplate.getForObject("http://localhost:8080/greeting?name=Gustavo", String.class);
+                System.out.println(message);
+            };
+        }
+}
---
+++ src/test/java/xyz/mulatodev/greetingclient/GreetingClientApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mulatodev.greetingclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
