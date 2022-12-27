a48325d7fca803d399de22a02210d73789558105
mulatodev <ganaranjo@gmail.com>
2022-12-27 16:35:19 -0500

    Update del commit inicial

--- src/main/java/xyz/mulatodev/greetingclient/GreetingClientApplication.java
+++ src/main/java/xyz/mulatodev/greetingclient/GreetingClientApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@RestController
 public class GreetingClientApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +23,7 @@ public class GreetingClientApplication {
         }
         
         @Bean
+        //@GetMapping("/hello")
         public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
             return arg -> {
                 String message = restTemplate.getForObject("http://localhost:8080/greeting?name=Gustavo", String.class);
